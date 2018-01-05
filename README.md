80ae7fafeadae0af6b8515bf42ebdfcf70e57cdd
Raouché <mohammad.jalloul@outlook.com>
2018-01-05 14:44:39 -0800

    Updated the functionality to account for nested folders in the blob storage account and replicating that to under the file share. Added plenty of additional logging. Updated README file.

--- README.md
+++ README.md
@@ -4,9 +4,9 @@ Transports media files from a watched Azure Storage account to a UNC file share.
 ## Deploying the template:
 To deploy the Azure Function App, click on the following button:
 
-[![Deploy to Azure](http://azuredeploy.net/deploybutton.png)](https://portal.azure.com/#create/Microsoft.Template/uri/https%3A%2F%2Fraw.githubusercontent.com%2FStratusOn%2FMedia-Transport-to-UNC%2Fmaster%2Fsrc%2FDeployments%2Fazuredeploy.json)
+[![Deploy to Azure (Consumption Plan)](http://azuredeploy.net/deploybutton.png)](https://portal.azure.com/#create/Microsoft.Template/uri/https%3A%2F%2Fraw.githubusercontent.com%2FStratusOn%2FMedia-Transport-to-UNC%2Fmaster%2Fsrc%2FDeployments%2Fazuredeploy.json)
 
 To deploy the Azure Function App with an App Service Plan instead of the default Consumption Plan (useful if you need bigger disks on the instances for example), click on the following button:
 
-[![Deploy to Azure](http://azuredeploy.net/deploybutton.png)](https://portal.azure.com/#create/Microsoft.Template/uri/https%3A%2F%2Fraw.githubusercontent.com%2FStratusOn%2FMedia-Transport-to-UNC%2Fmaster%2Fsrc%2FDeployments%2Fazuredeploy-withappplans.json)
+[![Deploy to Azure (App Service Plans)](http://azuredeploy.net/deploybutton.png)](https://portal.azure.com/#create/Microsoft.Template/uri/https%3A%2F%2Fraw.githubusercontent.com%2FStratusOn%2FMedia-Transport-to-UNC%2Fmaster%2Fsrc%2FDeployments%2Fazuredeploy-withappplans.json)
 
--- src/MediaTransport2UNC.FunctionApps/BlobToFileCopyUtility.cs
+++ src/MediaTransport2UNC.FunctionApps/BlobToFileCopyUtility.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Configuration;
+using System.Diagnostics;
 using System.IO;
 using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs.Host;
@@ -7,6 +8,7 @@ using Microsoft.WindowsAzure.Storage;
 using Microsoft.WindowsAzure.Storage.Blob;
 using Microsoft.WindowsAzure.Storage.File;
 
+// This class has no namespace to make it easier to include in both the local Function App project (compiled DLL, debug with VS) as well as a helper class to the CSX Function App C# script file.
 public static class BlobToFileCopyUtility
 {
     public static async Task CopyBlockBlobToFile(CloudBlockBlob sourceBlob, TraceWriter log)
@@ -17,21 +19,27 @@ public static class BlobToFileCopyUtility
             string targetConnectionString = ConfigurationManager.AppSettings["TARGET_STORAGEACCOUNT_CONNECTIONSTRING"];
             string fileShareName = ConfigurationManager.AppSettings["TARGET_FILESHARE_NAME"];
             string fileShareFolderName = ConfigurationManager.AppSettings["TARGET_FILESHARE_FOLDERNAME"];
-            // Assumes the name does not contain separators (e.g. "/" to designate nested folders):
             string targetFileName = sourceBlob.Name;
             bool deleteSourceBlobAfterTransferSuccess = bool.Parse(ConfigurationManager.AppSettings["DELETE_SOURCEBLOB_AFTER_TRANSFER_SUCCESS"]);
+
+            log.Info($"[Input]: Target File Name: {targetFileName}");
+            log.Info($"[Setting]: Target Connection String: {targetConnectionString}");
+            log.Info($"[Setting]: File Share Name: {fileShareName}");
+            log.Info($"[Setting]: File Share Folder Name: {fileShareFolderName}");
+            log.Info($"[Setting]: Delete Source Blob After Transfer Success: {deleteSourceBlobAfterTransferSuccess}");
 
             // 2) Copy to Azure Files.
             await CopyBlobToAzureFiles(sourceBlob, targetConnectionString, fileShareName, fileShareFolderName, targetFileName, log);
 
             if (deleteSourceBlobAfterTransferSuccess)
             {
+                log.Info($"Deleting source blob ({sourceBlob.Name}) after successful transfer since file share since DELETE_SOURCEBLOB_AFTER_TRANSFER_SUCCESS=true.");
                 await sourceBlob.DeleteAsync();
             }
         }
         catch (Exception exception)
         {
-            log.Error($"Error: {exception.Message}\n{exception.StackTrace}");
+            log.Error($"Error in CopyBlockBlobToFile: {exception.Message}\n{exception.StackTrace}");
         }
     }
 
@@ -43,28 +51,58 @@ public static class BlobToFileCopyUtility
 
         if (!share.Exists())
         {
+            log.Info($"Creating file share since it does not exist: {fileShareName}");
             await share.CreateAsync();
         }
 
         CloudFileDirectory rootDir = share.GetRootDirectoryReference();
         CloudFileDirectory targetDir = rootDir.GetDirectoryReference(fileShareFolderName);
         await targetDir.CreateIfNotExistsAsync();
-        CloudFile file = targetDir.GetFileReference(targetFileName);
         string localFilePath = GetLocalFilePath(sourceBlob.Name, log);
+        Stopwatch sw = new Stopwatch();
 
         try
         {
+            log.Info($"Downloading to local file: {localFilePath}");
+            sw.Start();
             await sourceBlob.DownloadToFileAsync(localFilePath, FileMode.Create);
-            log.Info($"Successfully downloaded to local file: {localFilePath}");
-            await file.UploadFromFileAsync(localFilePath);
-            log.Info($"Successfully uploaded local file to target share: {file.Uri.AbsoluteUri}");
+            sw.Stop();
+            log.Info($"Successfully downloaded (in {sw.ElapsedMilliseconds} msecs) to local file: {localFilePath}");
+
+            string[] sourceFolders = sourceBlob.Name.Split('/');
+            if (sourceFolders.Length > 1)
+            {
+                for (int i = 0; i < sourceFolders.Length - 1; i++)
+                {
+                    var subfolderName = sourceFolders[i];
+                    log.Info($"Creating subfolder: {subfolderName}");
+                    targetDir = targetDir.GetDirectoryReference(subfolderName);
+                    await targetDir.CreateIfNotExistsAsync();
+                }
+
+                CloudFile file = targetDir.GetFileReference(sourceFolders[sourceFolders.Length - 1]);
+                log.Info($"Uploading local file to target share under subfolder: {file.Uri.AbsoluteUri}");
+                sw.Restart();
+                await file.UploadFromFileAsync(localFilePath);
+                sw.Stop();
+                log.Info($"Successfully uploaded (in {sw.ElapsedMilliseconds} msecs) root local file to target share under subfolder: {file.Uri.AbsoluteUri}");
+            }
+            else
+            {
+                CloudFile file = targetDir.GetFileReference(targetFileName);
+                log.Info($"Uploading local file to target share root folder: {file.Uri.AbsoluteUri}");
+                sw.Restart();
+                await file.UploadFromFileAsync(localFilePath);
+                sw.Stop();
+                log.Info($"Successfully uploaded (in {sw.ElapsedMilliseconds} msecs) local file to target share root folder: {file.Uri.AbsoluteUri}");
+            }
         }
         finally
         {
             if (File.Exists(localFilePath))
             {
+                log.Info($"Deleting local file: {localFilePath}");
                 File.Delete(localFilePath);
-                log.Info($"Successfully deleted local file: {localFilePath}");
             }
         }
     }
@@ -73,13 +111,15 @@ public static class BlobToFileCopyUtility
     {
         string folderName = $"{new Random().Next(10000000, 99999999)}";
         string tempFolderPath = Path.Combine(Path.GetTempPath(), folderName);
-        if (!Directory.Exists(tempFolderPath))
+        string localFilePath = Path.Combine(tempFolderPath, fileName);
+        var localFile = new FileInfo(localFilePath);
+        if (!localFile.Directory.Exists)
         {
-            Directory.CreateDirectory(tempFolderPath);
+            log.Info($"Creating local temp directory: {localFile.Directory}");
+            localFile.Directory.Create();
         }
 
-        string localFilePath = Path.Combine(tempFolderPath, fileName);
-        log.Info($"Local File Path: {localFilePath}");
-        return localFilePath;
+        log.Info($"Local File Path: {localFile.FullName}");
+        return localFile.FullName;
     }
 }
